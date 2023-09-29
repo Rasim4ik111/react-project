95890f2c3fc3bc87b0d1ddf4bb51f3d5f3e393d0
Mirrasim Suleymanli <rasim95suleymanli@gmail.com>
2023-09-29 22:03:02 +0400

    added MyModal for create post

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import MyInput from "./components/UI/input/MyInput";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 
 const App = () => {
     const [posts, setPosts] = useState([{id: 1, title: "Javascript", body: "Javascript is a programming language"},])
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
 
     const sortedPosts = useMemo(() => {
         if (filter.sort) {
@@ -24,6 +27,7 @@ const App = () => {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -32,7 +36,12 @@ const App = () => {
 
 
     return (<div className="App">
-        <PostForm create={createPost} remove={removePost}/>
+        <MyButton onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost} remove={removePost}/>
+        </MyModal>
         <hr style={{marginBottom: "10px"}}/>
         <PostFilter
             filter={filter}
