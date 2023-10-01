3f17591cead00406f24c279ea047f66785d0fa10
Mirrasim Suleymanli <rasim95suleymanli@gmail.com>
2023-10-01 23:57:12 +0400

    add loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,39 +6,57 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 
 const App = () => {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
-    const [modal, setModal] = useState(false)
-    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
+    const [modal, setModal] = useState(false)     // for create post
+    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)     // custom hook
+    const [isPostsLoading, setIsPostsLoading] = useState(false)
 
+    useEffect(() => {
+        fetchPosts();
+    }, [filter])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
 
+    const fetchPosts = async () => {
+        setIsPostsLoading(true)
+        const posts = await PostService.getAll()
+        setPosts(posts)
+        setIsPostsLoading(false)
+    }
+    // Получаем post из дочернего компонента
     const removePost = (post) => {
         setPosts(posts.filter((p) => p.id !== post.id))
     }
 
 
-    return (<div className="App">
-        <MyButton onClick={() => setModal(true)}>
-            Создать пользователя
-        </MyButton>
-        <MyModal visible={modal} setVisible={setModal}>
-            <PostForm create={createPost} remove={removePost}/>
-        </MyModal>
-        <hr style={{marginBottom: "10px"}}/>
-        <PostFilter
-            filter={filter}
-            setFilter={setFilter}
-        />
-        <PostList remove={removePost} posts={sortedAndSearchPosts} title={"Posts List"}/>
-    </div>);
+    return (
+        <div className="App">
+            <MyButton onClick={() => setModal(true)}>
+                Создать пользователя
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost} remove={removePost}/>
+            </MyModal>
+            <hr style={{marginBottom: "10px"}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchPosts} title={"Posts List"}/>
+            }
+        </div>);
 };
 
 export default App;
