1017e80d94a5d71847241e1f287e76d837d967c3
Mirrasim Suleymanli <rasim95suleymanli@gmail.com>
2023-10-02 00:59:55 +0400

    something added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {usePosts} from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 
 const App = () => {
@@ -16,7 +17,10 @@ const App = () => {
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)     // for create post
     const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)     // custom hook
-    const [isPostsLoading, setIsPostsLoading] = useState(false)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll()
+        setPosts(posts)
+    })
 
     useEffect(() => {
         fetchPosts();
@@ -27,12 +31,7 @@ const App = () => {
         setModal(false)
     }
 
-    const fetchPosts = async () => {
-        setIsPostsLoading(true)
-        const posts = await PostService.getAll()
-        setPosts(posts)
-        setIsPostsLoading(false)
-    }
+
     // Получаем post из дочернего компонента
     const removePost = (post) => {
         setPosts(posts.filter((p) => p.id !== post.id))
@@ -52,6 +51,9 @@ const App = () => {
                 filter={filter}
                 setFilter={setFilter}
             />
+            {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
+            }
             {isPostsLoading
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
                 : <PostList remove={removePost} posts={sortedAndSearchPosts} title={"Posts List"}/>
